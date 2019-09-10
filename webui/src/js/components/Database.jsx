4d2b2b5840cca1795252f318ed08962106dda5be
Jorik Schellekens <joriks@matrix.org>
2019-09-10 13:54:03 +0100

    This move to a class is broken but backed up now.

--- webui/src/js/components/Database.jsx
+++ webui/src/js/components/Database.jsx
@@ -1,20 +1,4 @@
-/*
-Copyright 2018 The Matrix.org Foundation C.I.C.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-import React, { useState } from 'react';
+import React from 'react';
 
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
@@ -32,111 +16,109 @@ import { nextUI } from '../reducers/setup-ui-reducer';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 
-export default ({
-    onClick,
-}) => {
+const defaultDatabase = DATABASE_TYPES.POSTGRES;
+const defaultHost = "localhost"
 
-    const defaultDatabase = DATABASE_TYPES.POSTGRES;
-    const [database, setDatabase] = useState(defaultDatabase)
+class Database extends React.Component {
 
-    const hostDefault = "localhost"
-    const [databaseHost, setHost] = useState(hostDefault);
-    const [postgresDatabase, setPostgresDatabase] = useState();
-    const [databaseUsername, setUser] = useState();
-    const [databasePassword, setPassword] = useState();
+    constructor(props) {
 
-    const toggle = useAccordionToggle(nextUI(DATABASE_UI));
-
-    const submitPostrgess = () => {
-
-        toggle();
-        onClick({
-            databaseType: DATABASE_TYPES.POSTGRES,
-            databaseHost,
-            database: postgresDatabase,
-            databaseUsername,
-            databasePassword,
-        })
+        super(props);
+        this.state = {
+            databaseType: defaultDatabase,
+            databaseHost: defaultHost,
+            postgresDatabase: undefined,
+            databaseUsername: undefined,
+            databasePassword: undefined,
+            database: undefined,
+        }
 
     }
 
-    const submitSQLite3 = () => {
+    render() {
 
-        toggle();
-        onClick({
-            databaseType: DATABASE_TYPES.SQLITE3,
-        });
+        const onClick = (() => useAccordionToggle(nextUI(DATABASE_UI), this.props.onClick(this.state)));
+        return <Card>
+            <AccordionToggle as={Card.Header} eventKey={DATABASE_UI}>
+                Database
+            </AccordionToggle>
+            <Accordion.Collapse eventKey={DATABASE_UI}>
+                <Card.Body>
+                    <p>Synapse can use either SQLite3 or Postgres as it's database.</p>
+                    <p>Postgres is recommended.</p>
+
+                    <Tabs
+                        defaultActiveKey={defaultDatabase}
+                    >
+                        <Tab eventKey={DATABASE_TYPES.POSTGRES} title={"Postgres"}>
+                            This will connect to the given Postgres database via
+                            {DATABASE_TYPES.POSTGRES}
+                            <p>
+                                Host
+                            </p>
+                            <input
+                                type="text"
+                                onChange={e => this.setState({
+                                    databaseHost:
+                                        e.target.value ?
+                                            e.target.value :
+                                            hostDefault,
+                                })}
+                                autoFocus
+                                placeholder="localhost"
+                            />
+                            <p>
+                                Database name
+                            </p>
+                            <input
+                                type="text"
+                                onChange={e => this.setState({
+                                    database: e.target.value,
+                                })}
+                                placeholder="unspecified"
+                            />
+                            <p>
+                                User name
+                            </p>
+                            <input
+                                type="text"
+                                onChange={e => this.setState({
+                                    databaseUsername: e.target.value,
+                                })}
+                                placeholder="unspecified"
+                            />
+                            <p>
+                                Password
+                            </p>
+                            <input
+                                type="text"
+                                onChange={e => this.setState({
+                                    databasePassword: e.target.value,
+                                })}
+                                placeholder="unspecified"
+                            />
+                            <button
+                                className='inputButton'
+                                disabled={this.state.databaseHost ? undefined : true}
+                                onClick={onClick}
+                            >Use Postgres</button>
+                        </Tab>
+                        <Tab
+                            eventKey={DATABASE_TYPES.SQLITE3}
+                            title={DATABASE_TYPES.SQLITE3}
+                        >
+                            <button
+                                className='inputButton'
+                                onClick={onClick}
+                            >Use {DATABASE_TYPES.SQLITE3}</button>
+                        </Tab>
+                    </Tabs>
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card >
 
     }
-
-    return <Card>
-        <AccordionToggle as={Card.Header} eventKey={DATABASE_UI}>
-            Database
-        </AccordionToggle>
-        <Accordion.Collapse eventKey={DATABASE_UI}>
-            <Card.Body>
-                <p>Synapse can use either SQLite3 or Postgres as it's database.</p>
-                <p>Postgres is recommended.</p>
-
-                <Tabs defaultActiveKey={defaultDatabase} onSelect={k => setDatabase(k)}>
-                    <Tab eventKey={DATABASE_TYPES.POSTGRES} title={"Postgres"}>
-                        This will connect to the given Postgres database via
-                        {DATABASE_TYPES.POSTGRES}
-                        <p>
-                            Host
-                        </p>
-                        <input
-                            type="text"
-                            onChange={e => setHost(
-                                e.target.value ?
-                                    e.target.value :
-                                    hostDefault,
-                            )}
-                            autoFocus
-                            placeholder="localhost"
-                        />
-                        <p>
-                            Database name
-                        </p>
-                        <input
-                            type="text"
-                            onChange={e => setPostgresDatabase(e.target.value)}
-                            autoFocus
-                            placeholder="unspecified"
-                        />
-                        <p>
-                            User name
-                        </p>
-                        <input
-                            type="text"
-                            onChange={e => setUser(e.target.value)}
-                            autoFocus
-                            placeholder="unspecified"
-                        />
-                        <p>
-                            Password
-                        </p>
-                        <input
-                            type="text"
-                            onChange={e => setPassword(e.target.value)}
-                            autoFocus
-                            placeholder="unspecified"
-                        />
-                        <button
-                            className='inputButton'
-                            disabled={databaseHost ? undefined : true}
-                            onClick={submitPostrgess}
-                        >Use Postgres</button>
-                    </Tab>
-                    <Tab eventKey={DATABASE_TYPES.SQLITE3} title={DATABASE_TYPES.SQLITE3}>
-                        <button
-                            className='inputButton'
-                            onClick={submitSQLite3}
-                        >Use {DATABASE_TYPES.SQLITE3}</button>
-                    </Tab>
-                </Tabs>
-            </Card.Body>
-        </Accordion.Collapse>
-    </Card >
 
 }
+
+export default Database;
